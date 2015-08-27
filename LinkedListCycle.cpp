e526304505d8e8cbd7ef51ba9428e8476fd72d2f
Mohit Bhatia <mohitbhatia1994@gmail.com>
2015-08-27 19:43:07 -0400

    added 1 more

---
+++ LinkedListCycle.cpp
@@ -0,0 +1,32 @@
+// Given a linked list, determine if it has a cycle in it.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        
+        if (head == NULL)
+            return false;
+        if (head->next == head) {
+            return true;
+        }
+        
+        while  (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) 
+                return true;
+        }
+        return false;
+        
+    }
+};
