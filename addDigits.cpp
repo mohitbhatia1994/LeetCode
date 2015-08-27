cbebc03fb3fc6e6e74ff9ac89ef8d7cd326e51be
Mohit Bhatia <mohitbhatia1994@gmail.com>
2015-08-27 19:37:43 -0400

    added first question

---
+++ addDigits.cpp
@@ -0,0 +1,30 @@
+// Source : https://oj.leetcode.com/problems/
+// Author : Mohit Bhatia
+
+/********************************************************************************* 
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+For example:
+
+Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+*********************************************************************************/
+
+
+
+class Solution {
+public:
+    int addDigits(int num) {
+	    int sum ;
+	    if (num < 10) {return num;}
+	    while (num > 9) {
+        sum = 0;
+	    while(num != 0) {
+	     sum += num % 10;
+	     num -= num % 10;
+	     num /= 10;
+	    }
+	    num = sum;
+	   }
+      return sum;
+    }
+};
