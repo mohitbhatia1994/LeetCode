462177b00bf9065c11e46c074597a13c8e02adfd
Mohit Bhatia <mohitbhatia1994@gmail.com>
2015-08-27 19:41:35 -0400

    added 4 more

---
+++ SingleNumber.cpp
@@ -0,0 +1,14 @@
+// Given an array of integers, every element appears twice except for one. Find that single one.
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        std::sort (nums.begin(), nums.end());
+        for(int i = 0; i <= nums.size(); i = i + 2) {
+          if(nums[i] != nums[i+1]) {
+              return nums[i];
+          }  
+        }
+        return 0;
+    }
+};
