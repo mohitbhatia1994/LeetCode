0530d59e0e84077a65ed4a1eb2380f079e7b86b8
Mohit Bhatia <mohitbhatia1994@gmail.com>
2015-08-31 12:06:48 -0400

    Update NumberOf1Bits.cpp

--- NumberOf1Bits.cpp
+++ NumberOf1Bits.cpp
@@ -1,4 +1,3 @@
-// Source : https://oj.leetcode.com/problems/
 // Author : Mohit Bhatia
 
 // Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
