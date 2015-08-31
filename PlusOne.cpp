3f80442c950905fc908263e0072f3de0e0138762
Mohit Bhatia <mohitbhatia1994@gmail.com>
2015-08-31 12:07:07 -0400

    Update PlusOne.cpp

--- PlusOne.cpp
+++ PlusOne.cpp
@@ -1,8 +1,9 @@
 // Author: Mohit Bhatia
 
-/* Given a non-negative number represented as an array of digits, plus one to the number.
-
-The digits are stored such that the most significant digit is at the head of the list. */
+/* 
+Given a non-negative number represented as an array of digits, plus one to the number.
+The digits are stored such that the most significant digit is at the head of the list. 
+*/
 
 class Solution {
 public:
