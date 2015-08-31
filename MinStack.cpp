54be54b395d92b9df09f5dc15df0c76dc4f437ff
Mohit Bhatia <mohitbhatia1994@gmail.com>
2015-08-31 16:23:51 -0400

    Update MinStack.cpp

--- MinStack.cpp
+++ MinStack.cpp
@@ -18,14 +18,17 @@ public:
         if (sMin.empty() || x <= sMin.top())
             sMin.push(x);
     }
+    
     void pop() {
         if (s.top() <= sMin.top())
             sMin.pop();
         s.pop();
     }
+    
     int top() {
         return s.top();
     }
+    
     int getMin() {
         return sMin.top();
     }
