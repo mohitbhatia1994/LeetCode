b0b917f962105b6cc55d84f030256e886c852df0
Mohit Bhatia <mohitbhatia1994@gmail.com>
2015-09-03 00:21:28 -0400

    Update SearchInsertPosition.cpp

--- SearchInsertPosition.cpp
+++ SearchInsertPosition.cpp
@@ -1,3 +1,5 @@
+// Author: Mohit Bhatia
+
 // Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
 class Solution {
