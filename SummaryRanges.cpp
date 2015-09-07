159ca8162f7c515116fbf79e62c9fc56e43f9c79
Mohit Bhatia <mohitbhatia1994@gmail.com>
2015-09-07 00:39:18 -0400

    Update SummaryRanges.cpp

--- SummaryRanges.cpp
+++ SummaryRanges.cpp
@@ -1,6 +1,7 @@
+// Author: Mohit Bhatia
+
 /*
 Given a sorted integer array without duplicates, return the summary of its ranges.
-
 For example, given [0,1,2,4,5,7], return ["0->2","4->5","7"].
 */
 
