7bdb59df5c999456d04cb38828990cc4d6cf0271
Mohit Bhatia <mohitbhatia1994@gmail.com>
2015-10-09 00:14:15 -0400

    Update ValidAnagram.cpp

--- ValidAnagram.cpp
+++ ValidAnagram.cpp
@@ -2,10 +2,6 @@
 
 /*
 Given two strings s and t, write a function to determine if t is an anagram of s.
-
-For example,
-s = "anagram", t = "nagaram", return true.
-s = "rat", t = "car", return false.
 */
 
 class Solution {
